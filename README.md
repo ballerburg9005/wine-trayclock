c4c0b8dd03a69811fe8397493a6d5d24e53e8d01
ballerburg9005 <ballerburg9005@users.noreply.github.com>
2026-01-01 04:37:27 +0100

    .

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# wine-trayclock
-Windows application for Wine that creates a square digital clock in systray to keep "shell" desktop (taskbar mode) open, until you exit it from start menu.
+
+![Image](https://github.com/user-attachments/assets/c6f3c049-c8b7-4757-b23b-52312d0a0a78)
+
+# About
+
+This program makes sense in two ways:
+
+1. It prevents the virtual desktop from closing, unless you explicitly click Start->Exit desktop
+2. It displays the time (and also the date if you hover it)
+
+There are two ways to enable the taskbar in Wine (without which this program makes no sense):
+
+1. wine explorer /desktop=shell,1920x1080 # not recommended
+2. setting "shell" as default in regedit
+
+Go to `HKEY_CURRENT_USER\Software\Wine\Explorer` and set "Desktop" to "shell".
+In `HKEY_CURRENT_USER\Software\Wine\Explorer\Desktops` create a new String "shell" and set it to "1920x1080" or your preferred size.
+
+Now start wine like so:
+
+```wine "C:/trayclock.exe"```
+
+As noted, the virtual desktop will not close no matter what you do, unless you go to Start->Exit desktop. 
+
+# Compiling
+
+```
+x86_64-w64-mingw32-g++ -mwindows -static-libgcc -static-libstdc++ trayclock.cpp -o trayclock.exe 
+i686-w64-mingw32-g++ -mwindows -static-libgcc -static-libstdc++ trayclock.cpp -o trayclock32.exe
+
+```
+
+# Maximum Rice
+
+## Firejail, alternative wine version, DXVK, NVAPI, VKD3D & modeset for gaming
+
+```
+64BITWINE=; WINEEDIR=/opt/wine64; 0nvidia-reset off; WINERT=/opt/wine-10.11-fixed/ ;firejail --ignore=noinput  --private-etc=@x11,@games,@tls-ca,fonts --private=${WINEEDIR}/fakehome/ --private-dev --whitelist=/mnt/usb1 --whitelist=/mnt/2/GAMES --whitelist=/opt/STORE --whitelist=${WINERT} --whitelist=${WINEEDIR} --env=WINEARCH=win64 --env=WINEPREFIX=${WINEEDIR} --env=WINE=${WINERT}/bin/wine --env=XCURSOR_SIZE=16 --env=WINESERVER=${WINERT}/bin/wineserver --blacklist=/usr/local --blacklist=/bin --blacklist=/sbin --blacklist=/secret.tar.gz --blacklist=/srv --blacklist=/var/lib --blacklist=/var/www --blacklist=/root --blacklist=/boot --blacklist=/home/secret/ --env=VK_ICD_FILENAMES=/usr/share/vulkan/icd.d/nvidia_icd.json --env=VKD3D_FEATURE_LEVEL=12_2 --env=VKD3D_CONFIG=dxr12,dxr --env=WINE_ENABLE_NGX_UPDATER=1 --env=WINEESYNC=1 --env=VKD3D_DEBUG="none" --env=DXVK_ASYNC=1 --env=WINEDLLOVERRIDES="d3dcompiler_47=n;dxgi=n,b;d3d11=n,b" --env=WINE_ENABLE_NVAPI=1 --env=WINE_HIDE_NVIDIA_GPU=0 --env=WEBVIEW2_ADDITIONAL_BROWSER_ARGUMENTS="--no-sandbox" --env=XCURSOR_THEME=core --env=__GL_SYNC_TO_VBLANK=0 --env=__GL_MaxFramesAllowed=1 --env=__GL_GSYNC_ALLOWED=0 --env=__GL_VRR_ALLOWED=0 --env=VDPAU_NVIDIA_SYNC_DISPLAY_DEVICE=DP-4 --env=HOME=/opt/wine64/fakehome/ --env=DXVK_HUD=fps,cpu --env=__GL_THREADED_OPTIMIZATIONS=1 --env=WINEFSYNC=1 --net=none ${WINERT}/bin/wine "C:/trayclock.exe"; 0nvidia-reset
+```
+
+# Wallpaper
+
+```
+wine reg add "HKCU\Control Panel\Desktop" /v Wallpaper /t REG_SZ /d "C:\\path\\to\\image.bmp" /f
+wine reg add "HKCU\Control Panel\Desktop" /v WallpaperStyle /t REG_SZ /d 2 /f
+wine reg add "HKCU\Control Panel\Desktop" /v TileWallpaper /t REG_SZ /d 0 /f
+```
+
---
+++ trayclock.cpp
@@ -0,0 +1,181 @@
+#define UNICODE
+#define _UNICODE
+
+#include <windows.h>
+#include <shellapi.h>
+#include <stdint.h>
+#include <time.h>
+
+static const int ICON_W = 16;
+static const int ICON_H = 16;
+
+static HICON hIcon = nullptr;
+static NOTIFYICONDATAW nid = {};
+static HWND hWnd = nullptr;
+
+/*
+7px-high bitmap font, width = 4px per digit
+1 uses x=3 vertical stroke, foot width = 4
+*/
+static const uint8_t font[10][7] = {
+/*0*/ {0b1111, 0b1001, 0b1001, 0b1001, 0b1001, 0b1001, 0b1111},
+/*1*/ {0b0110, 0b0110, 0b0010, 0b0010, 0b0010, 0b0010, 0b1111},
+/*2*/ {0b1111, 0b0001, 0b0001, 0b1111, 0b1000, 0b1000, 0b1111},
+/*3*/ {0b1111, 0b0001, 0b0001, 0b1111, 0b0001, 0b0001, 0b1111},
+/*4*/ {0b1001, 0b1001, 0b1001, 0b1111, 0b0001, 0b0001, 0b0001},
+/*5*/ {0b1111, 0b1000, 0b1000, 0b1111, 0b0001, 0b0001, 0b1111},
+/*6*/ {0b1111, 0b1000, 0b1000, 0b1111, 0b1001, 0b1001, 0b1111},
+/*7*/ {0b1111, 0b0001, 0b0001, 0b0001, 0b0001, 0b0001, 0b0001},
+/*8*/ {0b1111, 0b1001, 0b1001, 0b1111, 0b1001, 0b1001, 0b1111},
+/*9*/ {0b1111, 0b1001, 0b1001, 0b1111, 0b0001, 0b0001, 0b1111}
+};
+
+static void DrawDigit(uint32_t* px, int ox, int oy, int digit, uint32_t color) {
+    for (int y = 0; y < 7; y++) {
+        uint8_t row = font[digit][y];
+        for (int x = 0; x < 4; x++) {
+            if (row & (1 << (3 - x))) {
+                int ix = ox + x;
+                int iy = oy + y;
+                if (ix >= 0 && ix < ICON_W && iy >= 0 && iy < ICON_H)
+                    px[iy * ICON_W + ix] = color;
+            }
+        }
+    }
+}
+
+static void UpdateIcon() {
+    SYSTEMTIME st;
+    GetLocalTime(&st);
+
+    uint32_t pixels[ICON_W * ICON_H];
+
+    const uint32_t BG = 0xFFF5F5F5;
+//    const uint32_t FG = 0xFF6A6A6A;
+    const uint32_t FG = 0xFF1E1E1E;
+
+    for (int i = 0; i < ICON_W * ICON_H; i++)
+        pixels[i] = BG;
+
+    int h1 = st.wHour / 10;
+    int h2 = st.wHour % 10;
+    int m1 = st.wMinute / 10;
+    int m2 = st.wMinute % 10;
+
+    // layout: 4px digit + 2px gap + 4px digit = 10px total
+    int total_w = 10;
+    int x0 = (ICON_W - total_w) / 2;
+
+    DrawDigit(pixels, x0,     0, h1, FG);
+    DrawDigit(pixels, x0 + 6, 0, h2, FG);
+    DrawDigit(pixels, x0,     8, m1, FG);
+    DrawDigit(pixels, x0 + 6, 8, m2, FG);
+
+    HBITMAP colorBmp = CreateBitmap(
+        ICON_W, ICON_H, 1, 32, pixels
+    );
+    HBITMAP maskBmp = CreateBitmap(
+        ICON_W, ICON_H, 1, 1, nullptr
+    );
+
+    ICONINFO ii = {};
+    ii.fIcon = TRUE;
+    ii.hbmColor = colorBmp;
+    ii.hbmMask = maskBmp;
+
+    if (hIcon)
+        DestroyIcon(hIcon);
+
+    hIcon = CreateIconIndirect(&ii);
+
+    DeleteObject(colorBmp);
+    DeleteObject(maskBmp);
+
+    nid.hIcon = hIcon;
+
+    wsprintfW(
+        nid.szTip,
+        L"%04d-%02d-%02d %02d:%02d",
+        st.wYear, st.wMonth, st.wDay,
+        st.wHour, st.wMinute
+    );
+
+    Shell_NotifyIconW(NIM_MODIFY, &nid);
+}
+
+static LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wp, LPARAM lp) {
+    if (msg == WM_TIMER)
+        UpdateIcon();
+    return DefWindowProc(hwnd, msg, wp, lp);
+}
+
+int WINAPI wWinMain(HINSTANCE hInst, HINSTANCE, PWSTR, int) {
+    WNDCLASSW wc = {};
+    wc.lpfnWndProc = WndProc;
+    wc.hInstance = hInst;
+    wc.lpszClassName = L"TrayClockWnd";
+    RegisterClassW(&wc);
+
+    hWnd = CreateWindowW(
+        wc.lpszClassName, L"",
+        WS_OVERLAPPED, 0, 0, 0, 0,
+        nullptr, nullptr, hInst, nullptr
+    );
+
+    nid.cbSize = sizeof(nid);
+    nid.hWnd = hWnd;
+    nid.uID = 1;
+    nid.uFlags = NIF_ICON | NIF_TIP;
+    nid.uCallbackMessage = WM_USER + 1;
+
+    UpdateIcon();
+    Shell_NotifyIconW(NIM_ADD, &nid);
+
+    SetTimer(hWnd, 1, 1000, nullptr);
+
+    MSG msg;
+    while (GetMessage(&msg, nullptr, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    Shell_NotifyIconW(NIM_DELETE, &nid);
+    if (hIcon) DestroyIcon(hIcon);
+    return 0;
+}
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, int) {
+    WNDCLASSW wc = {};
+    wc.lpfnWndProc = WndProc;
+    wc.hInstance = hInst;
+    wc.lpszClassName = L"TrayClockWnd";
+    RegisterClassW(&wc);
+
+    hWnd = CreateWindowW(
+        wc.lpszClassName, L"",
+        WS_OVERLAPPED, 0, 0, 0, 0,
+        nullptr, nullptr, hInst, nullptr
+    );
+
+    nid.cbSize = sizeof(nid);
+    nid.hWnd = hWnd;
+    nid.uID = 1;
+    nid.uFlags = NIF_ICON | NIF_TIP;
+    nid.uCallbackMessage = WM_USER + 1;
+
+    UpdateIcon();
+    Shell_NotifyIconW(NIM_ADD, &nid);
+
+    SetTimer(hWnd, 1, 1000, nullptr);
+
+    MSG msg;
+    while (GetMessage(&msg, nullptr, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    Shell_NotifyIconW(NIM_DELETE, &nid);
+    if (hIcon) DestroyIcon(hIcon);
+    return 0;
+}
+
